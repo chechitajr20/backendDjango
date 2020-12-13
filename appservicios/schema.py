59e8567f9154c6f6bf544e860340817abf130df6
Kevin Gonzalez <kevin17@mesoamericana.edu.gt>
2020-12-12 21:39:44 -0600

    escritura y actualizacion de servicios, contratos y usuarios

--- appservicios/schema.py
+++ appservicios/schema.py
@@ -144,9 +144,149 @@ class UpdateCliente(graphene.Mutation):
             return UpdateCliente(ok=ok, cliente=cliente_instance)
         return UpdateCliente(ok=ok, cliente=None)
 
+class CreateServicio(graphene.Mutation):
+    class Arguments:
+        input = ServicioInput(required=True)
+    
+    ok = graphene.Boolean()
+    servicio = graphene.Field(ServiciosType)
+
+    @staticmethod
+    def mutate(root, info, input=None):
+        ok = True
+        servicio_instance = Servicios(servicio = input.servicio, descripcion = input.descripcion)
+        servicio_instance.save()
+        return CreateServicio(ok=ok, servicio=servicio_instance)
+
+class UpdateServicio(graphene.Mutation):
+    class Arguments:
+        id = graphene.Int(required=True)
+        input = ServicioInput(required=True)
+    
+    ok = graphene.Boolean()
+    servicio = graphene.Field(ServiciosType)
+
+    @staticmethod
+    def mutate(root, info, id, input=None):
+        ok = False
+        servicio_instance = Servicios.objects.get(pk=id)
+        if servicio_instance:
+            ok = True
+            servicio_instance.servicio = input.servicio
+            servicio_instance.descripcion = input.descripcion
+            servicio_instance.save()
+            return UpdateServicio(ok=ok, servicio=servicio_instance)
+        return UpdateServicio(ok=ok, servicio=None)
+
+class CreateUsuario(graphene.Mutation):
+    class Arguments:
+        input = UsuarioInput(required=True)
+    
+    ok = graphene.Boolean()
+    usuario = graphene.Field(UsuariosType)
+
+    @staticmethod
+    def mutate(root, info, input=None):
+        ok = True
+        usuario_instance = Usuario(usuario=input.usuario, password=input.password)
+        usuario_instance.save()
+        return CreateUsuario(ok=ok, usuario=usuario_instance)
+
+class UpdateUsuario(graphene.Mutation):
+    class Arguments:
+        id = graphene.Int(required=True)
+        input = UsuarioInput(required=True)
+
+    ok = graphene.Boolean()
+    usuario = graphene.Field(UsuariosType)
+
+    @staticmethod
+    def mutate(root, info, id, input=None):
+        ok = False
+        usuario_instance = Usuario.objects.get(pk=id)
+        if usuario_instance:
+            ok = True
+            usuario_instance.usuario = input.usuario
+            usuario_instance.password = input.password
+            usuario_instance.save()
+            return UpdateUsuario(ok=ok, usuario=usuario_instance)
+        return UpdateUsuario(ok=ok, usuario=None)
+
+class CreateContratos(graphene.Mutation):
+    class Arguments:
+        input = ContratoInput(required=True)
+
+    ok = graphene.Boolean()
+    contrato = graphene.Field(ContratosType)
+
+    @staticmethod
+    def mutate(root, info, input=None):
+        ok = True
+        clientes = []
+        servicios = []
+
+        for cliente_input in input.clientes:
+            cliente = Cliente.objects.get(pk=cliente_input.id)
+            if cliente is None:
+                return CreateContratos(ok=False, contrato=None)
+            clientes.append(cliente)
+
+        for servicio_input in input.servicios:
+            servicio = Servicios.objects.get(pk=servicio_input.id)
+            if servicio is None:
+                return CreateContratos(ok=False, contrato=None)
+            servicios.append(servicio)
+
+        contrato_instance = Contratos(
+            fecha = input.fecha,
+            costo = input.costo
+            )
+        contrato_instance.save()
+        contrato_instance.clientes.set(clientes)
+        return CreateContratos(ok=ok, contrato=contrato_instance)
+
+class UpdateContrato(graphene.Mutation):
+    class Arguments:
+        id = graphene.Int(required=True)
+        input = ContratoInput(required=True)
+    ok = graphene.Boolean()
+    contrato = graphene.Field(ContratosType)
+
+    @staticmethod
+    def mutate(root,info,id,input=None):
+        ok = False
+        contrato_instance = Contratos.objects.get(pk=id)
+        if contrato_instance:
+            ok = True
+            clientes = []
+            servicios = []
+            for cliente_input in input.clientes:
+                cliente = Cliente.objects.get(pk=cliente_input.id)
+                if cliente is None:
+                    return UpdateContrato(ok=False,contrato=None)
+                clientes.append(cliente)
+            
+            for servicio_input in input.servicios:
+                servicio = Servicios.objects.get(pk=servicio_input.id)
+                if servicio is None:
+                    return UpdateContrato(ok=False, contrato=None)
+                servicios.append(servicio)
+            
+            contrato_instance.fecha = input.fecha
+            contrato_instance.costo = input.costo
+            contrato_instance.save()
+            contrato_instance.clientes.set(clientes)
+            contrato_instance.servicios.set(servicios)
+            return UpdateContrato(ok=ok, contrato=contrato_instance)
+        return UpdateContrato(ok=ok, contrato=None)
 
 class Mutation(graphene.ObjectType):
     create_cliente = CreateCliente.Field()
     update_cliente = UpdateCliente.Field()
-
+    create_servicio = CreateServicio.Field()
+    update_servicio = UpdateServicio.Field()
+    create_usuario = CreateUsuario.Field()
+    update_usuario = UpdateUsuario.Field()
+    create_contrato = CreateContratos.Field()
+    update_contrato = UpdateContrato.Field()
 schema = graphene.Schema(query=Query, mutation = Mutation)
