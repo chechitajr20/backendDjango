af9ad221fb9c380f361d283417a99dadcc6ff427
daga456 <diegoparedes2@outlook.es>
2020-12-13 11:58:28 -0600

    Pruebas0

--- appservicios/tests.py
+++ appservicios/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
-# Create your tests here.
+from appservicios.models import Usuario
+
+Class UsuarioModelTest(TestCase):
+    @classmethod
+    def test_usurio_max_length(self):
+        usuario=Usuario.objects.get(id=1)
+        max_length = Usuario._meta.get_field('usuario').max_length
+        self.assertEquals(max_length,30)
