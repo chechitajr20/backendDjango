7536765d3f5e784959a5b1ae8cb664d4d373fb49
Kevin Gonzalez <kevin17@mesoamericana.edu.gt>
2020-12-13 14:23:15 -0600

    nuevo modulo

--- servicios/urls.py
+++ servicios/urls.py
@@ -18,8 +18,8 @@ from django.urls import path
 from graphene_django.views import GraphQLView
 from django.views.decorators.csrf import csrf_exempt 
 from appservicios.schema import schema
-from django.config import settings
-from django.config.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
